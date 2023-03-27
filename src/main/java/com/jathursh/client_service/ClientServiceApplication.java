3e9d4e907cf2a3c1d3603f066e7c790966813902
Justy-11 <jathursh11@gmail.com>
2023-03-27 16:10:59 +0530

    added client service apis

--- src/main/java/com/jathursh/client_service/ClientServiceApplication.java
+++ src/main/java/com/jathursh/client_service/ClientServiceApplication.java
@@ -2,6 +2,9 @@ package com.jathursh.client_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClientServiceApplication {
@@ -10,4 +13,8 @@ public class ClientServiceApplication {
 		SpringApplication.run(ClientServiceApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
 }
--- src/main/java/com/jathursh/client_service/FileController.java
+++ src/main/java/com/jathursh/client_service/FileController.java
@@ -2,16 +2,21 @@ package com.jathursh.client_service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
 
 @RestController
 public class FileController {
@@ -22,10 +27,13 @@ public class FileController {
 
     String baseUrl = "http://localhost:9090";
 
-    @GetMapping("getAndSave/{fileName}")
+    // check "http://localhost:8080/getAndSave/{fileName}"
+    // {fileName} - in the fileStorage dir. of fileUploadDownload service
+    // get the selected file from the fileUploadDownload service dir. and save to this client service dir. (temp)
+    @GetMapping("/getAndSave/{fileName}")
     String getAndSaveFile(@PathVariable String fileName) throws IOException {
 
-        Resource resource = restTemplate.getForObject(baseUrl+ "/download" + fileName, Resource.class);
+        Resource resource = restTemplate.getForObject(baseUrl+ "/download/" + fileName, Resource.class);
 
         Path path = Paths.get("temp");
         Files.createDirectories(Paths.get("temp"));
@@ -33,6 +41,29 @@ public class FileController {
         Files.copy(resource.getInputStream(), Paths.get(path + "\\" + fileName), StandardCopyOption.REPLACE_EXISTING);
 
         return "data retrieved properly";
+    }
+
+    // http://localhost:9090//multiple/upload?files="1.jpg"&files="2.jpg"
+    // check  http://localhost:8080/executeTask
+    @GetMapping("/executeTask")
+    String executeTask() throws IOException {
+
+        // get the files from the location (hardcoded the location)
+        Stream<Path> list = Files.list(Paths.get("C:\\Users\\JATHURSHAN\\Pictures\\toUpload"));
+
+        MultiValueMap filesToUpload = new LinkedMultiValueMap<>();
+
+        list.forEach(
+                file ->{
+                    try {
+                        filesToUpload.add("files", new UrlResource(file.toAbsolutePath().toUri()));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        return restTemplate.postForObject(baseUrl + "/multiple/upload", filesToUpload, String.class);  // uploaded to the fileStorage dir. of fileUploadDownload service
     }
 
 }
